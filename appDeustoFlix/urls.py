f6fb77448278fe59e080a5fcc156a98cf31ac688
anemugarza <ane.mugarza@opendeusto.es>
2023-11-27 19:22:13 +0100

    clases basadas en vistas hecho

--- appDeustoFlix/urls.py
+++ appDeustoFlix/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.indexPortada, name='indexPortada'),
     path('peliculas/<int:pk>/', views.ShowPelicula.as_view(), name='ShowPelicula'),
-    path('directores/<int:director_id>/', views.ShowDirector.as_view(), name='ShowDirector'),
-    path('generos/<int:genero_id>/', views.ShowGenero.as_view(), name='ShowGenero'),
-    path("peliculas/", views.IndexPeliculas.as_view(), name="IndexPeliculas"),
+    path('directores/<int:pk>/', views.ShowDirector.as_view(), name='ShowDirector'),
+    path('generos/<int:pk>/', views.ShowGenero.as_view(), name='ShowGenero'),
+    path('peliculas/', views.IndexPeliculas.as_view(), name="IndexPeliculas"),
     path("generos/", views.IndexGeneros.as_view(), name="IndexGeneros"),
     path("directores/", views.IndexDirectores.as_view(), name="IndexDirectores"),
 ]
+
--- appDeustoFlix/views.py
+++ appDeustoFlix/views.py
@@ -10,58 +10,59 @@ import json
 #devuelve el listado de generos
 class IndexGeneros(ListView):
 	model = Genero
-	queryset = Genero.objects.all()
+	template_name = 'generos.html'
+	queryset = Genero.objects.order_by('nombre')
+
+
 
 #devuelve los datos de un genero
 class ShowGenero(DetailView):
 	model = Genero
-
-'''def get(request, genero_id):
-	genero = get_object_or_404(Genero, pk=genero_id)
-	peliculasGenero = genero.pelicula_set.all()
-	context = {'genero': genero ,'peliculasGenero' : peliculasGenero }	
-	return render(request, 'genero.html', context)'''
+	template_name = 'genero.html'
+	def get_context_data(self, **kwargs):
+			context = super().get_context_data(**kwargs)
+			genero = self.get_object()
+			context['peliculasGenero'] = genero.pelicula_set.all()
+			return context
 
 
 #devuelve el listado de peliculas
 class IndexPeliculas(ListView):
 	model = Pelicula
-	queryset = Pelicula.objects.all()
+	queryset = Pelicula.objects.order_by('nombre')
+	template_name = 'peliculas.html'
+
+
 
 #devuelve los datos de una pelicula
 class ShowPelicula(DetailView):
-	model = Pelicula
-	template = 'pelicula.html'
-	context_object_name = 'pelicula'
-	pk = 'pelicula_id'
-
-'''def show_pelicula(request, pelicula_id):
-	pelicula = get_object_or_404(Pelicula, pk=pelicula_id)
-	context = {'pelicula': pelicula }
-	return render(request, 'pelicula.html', context)'''
-
+	model = Pelicula		
+	template_name = 'pelicula.html'
+	
 #devuelve el listado de directores
 class IndexDirectores(ListView):
 	model = Director
-	queryset = Director.objects.all()
+	queryset = Director.objects.order_by('-nombre')
+	template_name = 'directores.html'
+
+
 
 #devuelve los datos de un director
 class ShowDirector(DetailView):
 	model = Director
+	template_name = 'director.html'
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		director = self.get_object()
+		context['peliculasDirector'] = director.pelicula_set.all()
+		return context
 
-'''def get(request, director_id):
-	director = get_object_or_404(Director, pk=director_id)
-	peliculasDirector = director.pelicula_set.all()
-	context = {'director': director , 'peliculasDirector':peliculasDirector}
-	return render(request, 'director.html', context)
-'''
 
 #Devuelve la pelicula mas vista de cada genero
 def indexPortada(request):
-	generos = Genero.objects.all()
 	peliculasMasVistas = []  # Una lista de objetos de modelo Pelicula
 
-	for genero in generos:
+	for genero in Genero.objects.all():
 		pelicula_mas_vista = Pelicula.objects.filter(genero=genero).order_by('-vecesVista').first()
 		if pelicula_mas_vista:
 			peliculasMasVistas.append(pelicula_mas_vista)
