afd0fb28404aabcb0824dfbffbc21d44cea52d99
marMugica <mar.mugica@opendeusto.es>
2023-11-30 16:49:02 +0100

    no añadir campos vacios vue

--- agendaVue/js/agenda.js
+++ agendaVue/js/agenda.js
@@ -8,25 +8,23 @@ new Vue({
     el: "#contactos",
     data: {
         contactos: [contactoEjemplo],
-        nuevoContacto: {
-            "nombre": "",
-            "email": "",
-            "telf": ""
-        }
+        nuevoContacto: {"nombre": "", "email": "", "telf": ""}
     },
     methods: {
         anyadirContacto() {
-            this.contactos.push({
-                "nombre": this.nuevoContacto.nombre,
-                "email": this.nuevoContacto.email,
-                "telf": this.nuevoContacto.telf
-            });
-
-            this.nuevoContacto = {
-                "nombre": "",
-                "email": "",
-                "telf": ""
-            };
+            if (
+              this.nuevoContacto.nombre.trim() !== '' &&
+              this.nuevoContacto.email.trim() !== '' &&
+              this.nuevoContacto.telf.trim() !== ''
+            ) {
+              this.contactos.push({
+                nombre: this.nuevoContacto.nombre,
+                email: this.nuevoContacto.email,
+                telf: this.nuevoContacto.telf
+              });
+      
+              this.nuevoContacto = { nombre: '', email: '', telf: '' };
+            }
         },
         borrarContacto(indice) {
             this.contactos.splice(indice, 1);
