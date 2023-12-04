83408aeb3af13896e35b578b41c9124f26f93363
Imanol <alonso.imanol@opendeusto.es>
2023-12-04 12:55:39 +0100

    i18n

--- deustoFlix/urls.py
+++ deustoFlix/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
     path('', include('appDeustoFlix.urls')),  
     path('admin/', admin.site.urls),
 ]
 
+urlpatterns = i18n_patterns(
+    path('', include('appDeustoFlix.urls')),  
+    path('admin/', admin.site.urls),
+)
