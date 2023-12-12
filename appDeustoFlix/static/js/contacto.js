a71a09671ea3e98730fb06a0a1f726cddb616379
anemugarza <ane.mugarza@opendeusto.es>
2023-12-12 15:54:24 +0100

    borrado

--- appDeustoFlix/static/js/contacto.js
+++
@@ -1,9 +0,0 @@
-// Muestra una alerta en el navegador informando de que el mensaje ha sido recibido.
-$(document).ready(
-    function () {
-        document.getElementById("btn_enviar").onclick = function () {
-            window.alert("Su mensaje ha sido recibido. Nos pondremos en contacto con usted.");
-        };
-    }
-);
-
